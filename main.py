34fa6c50ad5d0cc41702f7655c4c3d0254b1a54b
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-10-30 12:50:57 -0500

    solucion del ejercicio años bisiestos

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-#Escriba un programa que determine si el número entero ingresado por el usuario es par o no.
+#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más.
+#  Más precisamente, la diferencia es de más o menos un cuarto de día.
 
-#Ingrese un número: 4
-#Su número es par
-#Ingrese un número: 3
-#Su número es impar
+#Para evitar que las estaciones se desfasen con el calendario,
+# el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados bisiestos), 
+# para tomar en consideración los cuatro cuartos de día acumulados.
 
-numero = int(input("Ingrese un número: "))
+#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
 
-if numero % 2 == 0:
-    print("Su número es par")
+#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, 
+# en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que fuera divisible por 400.
+
+#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+
+año = int(input("Ingrese un año: "))
+
+if año < 1582:
     
+    if año % 4 == 0:
+        print(f"El año {año} es bisiesto según el calendario juliano.")
+
+    else:
+        print(f"El año {año} no es bisiesto según el calendario juliano.")
+        
 else:
-    print("Su número es impar")
+   
+    if (año % 4 == 0 and (año % 100 != 0 or año % 400 == 0)):
+        print(f"El año {año} es bisiesto según el calendario gregoriano.")
+
+    else:
+        print(f"El año {año} no es bisiesto según el calendario gregoriano.")
+
